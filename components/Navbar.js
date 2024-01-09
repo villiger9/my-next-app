ff71c69dbe3737251285b04e3970c3f002aad193
villiger9 <ali.shaker93@gmail.com>
2024-01-09 17:01:16 +0300

    navbar scroll action and css changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,28 @@
 // components/Navbar.js
+import { useEffect, useState } from 'react';
 import styles from '../styles/navbar.module.css';
 
 const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos, visible]);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${visible ? '' : styles.hidden}`}>
       <a href="/">Home</a>
       <a href="/table">Table</a>
       <a href="/cards">Cards</a>
